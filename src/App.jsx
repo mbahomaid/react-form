8d20b57fcb9d459bd17ff8d928452e3e7c70e2a0
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-01-13 20:32:31 +0300

    Build the form component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Form from "./components/Form";
+
 function App() {
   return (
     <>
-    <h1 className="text-sm">React Form</h1>
+      <h1 className="text-blue-700 text-center my-5 text-5xl">React Form</h1>
+      <Form/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,86 @@
 function Form() {
   return (
     <>
-    
+      <form className="w-full max-w-lg center mx-auto mt-10">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              First Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-email"
+            >
+              Email
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-password"
+              type="email"
+              placeholder="jane@doe.com"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              Password
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-gray-600 text-xs italic">
+              Make sure to write a strong password
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3 flex justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default Form
+export default Form;
